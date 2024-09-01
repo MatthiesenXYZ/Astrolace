7c8e6fed16fa76459d02b40d5cd71f6b63c8a3c4
Adam Matthiesen <amatthiesen@outlook.com>
2024-08-31 23:34:56 -0700

    chore: Update props interface and exports for 3 components

---
+++ .changeset/gentle-games-matter.md
@@ -0,0 +1,5 @@
+---
+"@matthiesenxyz/astrolace": patch
+---
+
+fix. update props interface on 3 components and update exports
---
+++ package/src/components/index.ts
@@ -0,0 +1,62 @@
+// Folders
+export { default as Breadcrumb } from "./breadcrumbs/Breadcrumb.astro";
+export { default as BreadcrumbItem } from "./breadcrumbs/BreadcrumbItem.astro";
+export { default as Button } from "./buttons/Button.astro";
+export { default as ButtonGroup } from "./buttons/ButtonGroup.astro";
+export { default as CopyButton } from "./buttons/CopyButton.astro";
+export { default as Carousel } from "./carousel/Carousel.astro";
+export { default as CarouselItem } from "./carousel/CarouselItem.astro";
+export { default as FormatBytes } from "./formatters/FormatBytes.astro";
+export { default as FormatDate } from "./formatters/FormatDate.astro";
+export { default as FormatNumber } from "./formatters/FormatNumber.astro";
+export { default as Icon } from "./icon/Icon.astro";
+export { default as IconButton } from "./icon/IconButton.astro";
+export { default as Menu } from "./menu/Menu.astro";
+export { default as MenuItem } from "./menu/MenuItem.astro";
+export { default as MenuLabel } from "./menu/MenuLabel.astro";
+export { default as Radio } from "./radios/Radio.astro";
+export { default as RadioGroup } from "./radios/RadioGroup.astro";
+export { default as RadioButton } from "./radios/RadioButton.astro";
+export { default as Option } from "./select/Option.astro";
+export { default as Select } from "./select/Select.astro";
+export { default as Tab } from "./tabs/Tab.astro";
+export { default as TabGroup } from "./tabs/TabGroup.astro";
+export { default as TabPanel } from "./tabs/TabPanel.astro";
+export { default as Tree } from "./tree/Tree.astro";
+export { default as TreeItem } from "./tree/TreeItem.astro";
+
+// Custom ShoelaceHeader for registering icons
+export { default as ShoelaceHeader } from "./header/ShoelaceHeader.astro";
+
+// Misc components
+export { default as Alert } from "./Alert.astro";
+export { default as AnimatedImage } from "./AnimatedImage.astro";
+export { default as Avatar } from "./Avatar.astro";
+export { default as Badge } from "./Badge.astro";
+export { default as Card } from "./Card.astro";
+export { default as Checkbox } from "./Checkbox.astro";
+export { default as ColorPicker } from "./ColorPicker.astro";
+export { default as Details } from "./Details.astro";
+export { default as Dialog } from "./Dialog.astro";
+export { default as Divider } from "./Divider.astro";
+export { default as Drawer } from "./Drawer.astro";
+export { default as Dropdown } from "./Dropdown.astro";
+export { default as ImageComparer } from "./ImageComparer.astro";
+export { default as Include } from "./Include.astro";
+export { default as Input } from "./Input.astro";
+export { default as MutationObserver } from "./MutationObserver.astro";
+export { default as Popup } from "./Popup.astro";
+export { default as ProgressBar } from "./ProgressBar.astro";
+export { default as ProgressRing } from "./ProgressRing.astro";
+export { default as Range } from "./Range.astro";
+export { default as Rating } from "./Rating.astro";
+export { default as RelativeTime } from "./RelativeTime.astro";
+export { default as ResizeObserver } from "./ResizeObserver.astro";
+export { default as Skeleton } from "./Skeleton.astro";
+export { default as Spinner } from "./Spinner.astro";
+export { default as SplitPanel } from "./SplitPanel.astro";
+export { default as Switch } from "./Switch.astro";
+export { default as Tag } from "./Tag.astro";
+export { default as Textarea } from "./Textarea.astro";
+export { default as Tooltip } from "./Tooltip.astro";
+export { default as VisuallyHidden } from "./VisuallyHidden.astro";
